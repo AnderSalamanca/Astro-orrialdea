c2f8655814fed1717b80e0a7d37b64298089cadc
AnderSalamanca <ander3223@gmail.com>
2025-07-17 14:58:21 +0200

    Empezamos con los restaurantes

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,29 @@ const projectCollection = defineCollection({
   }),
 });
 
+const restaurantCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    location: z.string(),
+    coordinates: z.object({
+      latitude: z.number(),
+      longitude: z.number(),
+    }).optional(),
+    cuisine: z.array(z.enum(['Italiana', 'Mexicana', 'Japonesa', 'Vasca', 'Española', 'Mediterránea'])).nonempty(),
+    rating: z.number().min(0).max(5),
+    priceRange: z.enum(['€', '€€', '€€€', '€€€€']).optional(),
+    image: image(),
+    imageAlt: z.string(),
+    description: z.string(),
+    gallery: z.array(z.object({
+      src: image(),
+      alt: z.string(),
+    })).optional(),
+  }),
+});
+
 export const collections = {
   'proyectos': projectCollection,
+  'restaurantes': restaurantCollection,
 };
---
+++ src/content/restaurantes/restaurante1.md
@@ -0,0 +1,13 @@
+---
+name: "Malum Rex"
+location: "Astigarraga"
+cuisine: 
+- Vasca
+rating: 4
+priceRange: "€€"
+image: "../../assets/weather-app.jpeg"
+imageAlt: "Restaurante Malum Rex"
+description: En el corazón de Astigarraga, cuna de la sidra, Malum Rex ('el rey de la manzana') rinde homenaje al producto con una parrilla moderna y alma tradicional. Aquí, la txuleta a la brasa es tratada con maestría, ofreciendo una experiencia vasca auténtica y refinada.
+
+---
+
